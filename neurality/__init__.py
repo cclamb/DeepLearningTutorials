19db1c13932b5054a43859fcb00078d93f08496b
cclamb <chrislambistan@gmail.com>
2015-07-03 12:49:13 -0600

    organizing MLP work

--- neurality/__init__.py
+++ neurality/__init__.py
@@ -3,3 +3,4 @@ __author__ = 'cclamb'
 from inputpreprocessor import InputPreprocessor
 from logisticregression import LogisticRegression
 from logisticregression import sgd_optimization_mnist
+from multilayerperceptron import HiddenLayer, MultilayerPerceptron, test_mlp
